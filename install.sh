0c4294a8a50b2a1e50c8f9c9cd54dcb2a42e4931
Maicon Matos <maicon@tarken.ag>
2025-11-18 22:20:01 -0300

    Add installation script for kubectl and kind

---
+++ install.sh
@@ -0,0 +1,17 @@
+#Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+#Create kubectl alias k
+echo "alias k='kubectl'" >> ~/.zshrc
+source ~/.zshrc
+
+#Install kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+kind version
+
+#Create a kind cluster
+kind create cluster --name my-cluster
