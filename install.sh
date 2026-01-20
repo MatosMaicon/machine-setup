a081b782afd30112447e7deb8a04556818c0d401
Maicon Matos <maicon@tarken.ag>
2026-01-20 19:33:07 -0300

    Enhance installation script: add helm installation and kubectl completion for zsh

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ kubectl version --client
 
 #Create kubectl alias k
 echo "alias k='kubectl'" >> ~/.zshrc
+echo "source <(kubectl completion zsh)" >> ~/.zshrc
 source ~/.zshrc
+
+#Install helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+helm version
 
 #Install kind
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
@@ -13,5 +18,7 @@ chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 kind version
 
-#Create a kind cluster
-kind create cluster --name my-cluster
+#Install kustomize
+#curl -L https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.2.1/kustomize_v5.2.1_linux_amd64.tar.gz | sudo tar xz -C /usr/local/bin
+#kustomize version
+
